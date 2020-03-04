f8dcdc20a4aeada08f262aa2da2c2dea6853be60
nibortolum <matthieu.mulot@datatailors.ch>
2020-03-04 07:17:17 +0100

    added p value corrections

--- R/generate_adjmat.R
+++ R/generate_adjmat.R
@@ -1,15 +1,24 @@
 #' Compute Adjacency Matrix from Cooccur object
 #'
 #' @param cooccur A cooccur object retruned by fast_cooccur
+#' @param p.adj a correction method for the p.value
+#' @param verbose TRUE or FALSE. If TRUE, display meaningful messages about the progression
 #'
 #' @return A signed adjacency matrix
 #' @export
 #'
 #' @examples
-generate_adjmat <- function(cooccur){
+generate_adjmat <- function(cooccur, p.adj = "none", verbose = TRUE){
   if(class(cooccur)!= "cooccur"){
     stop("coccur must be a cooccur object")
   }
+  if(p.adj != "none"){
+
+    if(verbose) {cat("correction pvalues \n")}
+    cooccur$results$p_gt <- p.adjust(cooccur$results$p_gt, method = p.adj)
+    cooccur$results$p_lt <- p.adjust(cooccur$results$p_lt, method = p.adj)
+  }
+  if(verbose){cat("Rendering adjacency matrix\n")}
   positive <- as.matrix(cooccur$results[which(cooccur$results$p_gt < 0.05) ,c("spp", "spp_next")])
   negative <- as.matrix(cooccur$results[which(cooccur$results$p_lt < 0.05) ,c("spp", "spp_next")])
 
